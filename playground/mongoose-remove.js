3ffafffc35199ebd50f6263effbf5a9f8945a392
Adam Derenbecher <adam.derenbecher@gmail.com>
2018-01-22 12:42:48 -0600

    adding DELETE Todo route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5a662bf48e8b5d1f404057db').then((todo) => {
+  console.log(todo);
+});
