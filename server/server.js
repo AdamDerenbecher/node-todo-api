51920d194fea7099f7ba2ac91df14d73cf277e6e
Adam Derenbecher <adam.derenbecher@gmail.com>
2018-01-22 11:13:53 -0600

    add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get('/todos/:id', (req, res) => {
   }
   Todo.findById(id).then((todo) => {
     if (todo) {
-      res.status(400).send({todo});
+      res.status(200).send({todo});
     } else {
       return res.status(404).send();
     }
