15b448538699b4dcde0700e9b8f8ad1f6759446a
Adam Derenbecher <adam.derenbecher@gmail.com>
2018-01-24 15:36:48 -0600

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -265,3 +265,58 @@ describe('POST /users/', () => {
       done();
   });
 });
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.header['x-auth']).toExist();
+      })
+      .end((err,res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((thisUser) => {
+          expect(thisUser.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => {
+          done(e);
+        })
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password + '1'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.header['x-auth']).toNotExist();
+      })
+      .end((err,res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((thisUser) => {
+          expect(thisUser.tokens.length).toBe(0);
+          done();
+        }).catch((e) => {
+          done(e);
+        })
+      });
+  });
+});
